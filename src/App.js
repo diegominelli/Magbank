90dafa7bd315b1af6dd3f6a87371b671bcad9c80
Diego Minelli <diego.minelli@hotmail.com>
2021-05-09 02:09:41 -0300

    Criando o CardList e o CenteredButton

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import CreditCard from "./components/CreditCard";
+import CardList from "./components/CardList";
+import CenteredButton from "./components/CenteredButton";
 import "./App.scss";
+
+import posts from "./data/posts";
 
 const App = () => (
   <div className="App">
@@ -10,6 +14,8 @@ const App = () => (
     <Hero />
 
     <CreditCard />
+    <CardList posts={posts} />
+    <CenteredButton>Abra sua conta</CenteredButton>
   </div>
 );
 
---
+++ src/data/posts.js
@@ -0,0 +1,32 @@
+import image1 from "../assets/post-1.jpg";
+import image2 from "../assets/post-2.jpg";
+import image3 from "../assets/post-3.jpg";
+
+const posts = [
+  {
+    id: 1,
+    image: image1,
+    title: "Loren ipsum dolor sit",
+    excerpt:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    action: "Peça já",
+  },
+  {
+    id: 2,
+    image: image2,
+    title: "Phasellus eget elementum diam",
+    excerpt:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    action: "Peça já",
+  },
+  {
+    id: 3,
+    image: image3,
+    title: "Fusce Tempus",
+    excerpt:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    action: "Peça já",
+  },
+];
+
+export default posts;
