cc74b1f8de4d38d62bd4d33a5cb5c44416ef4296
Diego Minelli <diego.minelli@hotmail.com>
2021-05-08 23:39:56 -0300

    Utilizando as classes utilitarias

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Jumbotron, Button, Image } from "react-bootstrap";
+import { Row, Col, Jumbotron, Button, Image } from "react-bootstrap";
 import logo from "../assets/logo__magic-pay.png";
 
 const Hero = () => (
-  <Jumbotron className="text-center">
-    <Image src={logo} />
-    <p>Pague suas contas pelo nosso APP</p>
-    <Button variant="outline-light">Abra sua conta</Button>
+  <Jumbotron className="text-center text-light">
+    <Row className="my-lg-5">
+      <Col lg className="text-lg-right my-lg-5">
+        <Image src={logo} />
+      </Col>
+      <Col lg className="text-lg-left my-lg-5">
+        <p>Pague suas contas pelo nosso APP</p>
+        <Button variant="outline-light">Abra sua conta</Button>
+      </Col>
+    </Row>
   </Jumbotron>
 );
 
