896923d42eadcf662a19b45eb003af54fe594403
Diego Minelli <diego.minelli@hotmail.com>
2021-05-11 16:18:25 -0300

    Construindo o Faq

---
+++ src/components/Faq.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import IconText from './IconText';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCreditCard,
+  faWallet,
+  faShieldAlt,
+  faUserTie,
+} from '@fortawesome/free-solid-svg-icons';
+
+const Faq = () => (
+  <Container>
+    <Row>
+      <Col className="d-lg-none" xs={12}>
+        <Row>
+          <Col>
+            <FontAwesomeIcon icon={faCreditCard} size="2x" />
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faWallet} size="2x" />
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faShieldAlt} size="2x" />
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faUserTie} size="2x" />
+          </Col>
+        </Row>
+      </Col>
+      <Col className="d-none d-lg-block">
+        <IconText icon={faCreditCard} size={3}>
+          Cartão de crédito e débito
+        </IconText>
+        <IconText icon={faWallet} size={3}>
+          Conta e abertura
+        </IconText>
+        <IconText icon={faShieldAlt} size={3}>
+          Token digital
+        </IconText>
+        <IconText icon={faUserTie} size={3}>
+          Produtos e serviços
+        </IconText>
+      </Col>
+      <Col></Col>
+    </Row>
+  </Container>
+);
+
+export default Faq;
