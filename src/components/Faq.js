88641986d8d2bca489c38d8cf0e4e0af0d6f3d7f
Diego Minelli <diego.minelli@hotmail.com>
2021-05-19 13:59:16 -0300

    Refatorando e finalizando o Faq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container, Row, Col, Accordion, Card, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Accordion, Card } from 'react-bootstrap';
 import IconText from './IconText';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -11,144 +11,143 @@ import {
 
 import './Faq.scss';
 
-const Faq = () => (
-  <section className="faq text-light">
-    <Container className="py-5">
-      <Row className="justify-content-center">
-        <h2 className="faq-title my-5">Dúvidas frequentes</h2>
-      </Row>
-      <Row className="d-flex align-items-center justify-items-center">
-        <Col className="d-lg-none mb-5" xs={12}>
-          <Row>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faCreditCard} size="2x" color="#FFF" />
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faWallet} size="2x" color="#FFF" />
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faShieldAlt} size="2x" color="#FFF" />
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <FontAwesomeIcon icon={faUserTie} size="2x" color="#FFF" />
-            </Col>
-          </Row>
-        </Col>
-        <Col className="d-none d-lg-block">
-          <IconText
-            icon={faCreditCard}
-            size={3}
-            className="mb-3"
-            textClassName="lead"
-            color="#FFF"
-          >
-            Cartão de crédito e débito
-          </IconText>
-          <IconText
-            icon={faWallet}
-            size={3}
-            className="mb-3"
-            textClassName="lead"
-            color="#FFF"
-          >
-            Conta e abertura
-          </IconText>
-          <IconText
-            icon={faShieldAlt}
-            size={3}
-            className="mb-3"
-            textClassName="lead"
-            color="#FFF"
-          >
-            Token digital
-          </IconText>
-          <IconText icon={faUserTie} size={3} textClassName="lead" color="#FFF">
-            Produtos e serviços
-          </IconText>
-        </Col>
-        <Col>
-          <Accordion defaultActiveKey="0">
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+const Faq = () => {
+  const options = [
+    { icon: faCreditCard, text: 'Cartão de crédito' },
+    { icon: faWallet, text: 'Conta e abertura' },
+    { icon: faShieldAlt, text: 'Token digital' },
+    { icon: faUserTie, text: 'Produtos e Serviços' },
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  const handleClick = (key) => {
+    setIndex(key);
+  };
+
+  useEffect(() => {
+    console.log(index);
+  }, [index]);
+
+  return (
+    <section className="faq text-light">
+      <Container className="py-5">
+        <Row className="justify-content-center">
+          <h2 className="faq-title my-5">Dúvidas frequentes</h2>
+        </Row>
+        <Row className="d-flex align-items-center justify-items-center">
+          <Col className="d-lg-none mb-5" xs={12}>
+            <Row>
+              {options.map(({ icon }, key) => (
+                <Col className="d-flex justify-content-center" key={key}>
+                  <FontAwesomeIcon
+                    icon={icon}
+                    size="2x"
+                    color={key === index ? '#FFF' : '#BBB'}
+                    onClick={() => handleClick(key)}
+                  />
+                </Col>
+              ))}
+            </Row>
+            <Row className="d-flex justify-content-center mt-5">
+              <p className="lead">{options[index].text}</p>
+            </Row>
+          </Col>
+          <Col className="d-none d-lg-block">
+            {options.map(({ icon, text }, key) => (
+              <IconText
+                icon={icon}
+                size={3}
+                className="faq-clickable mb-3"
+                textClassName="lead"
+                color={key === index ? '#FFF' : '#BBB'}
+                onClick={() => handleClick(key)}
+                key={key}
+              >
+                {text}
+              </IconText>
+            ))}
+          </Col>
+          <Col>
+            <Accordion defaultActiveKey="0" activeKey={`${index}`}>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
                   Lorem ipsum dolor sit amet
                 </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                  dignissim. Nam eget quam semper, varius ligula sed, auctor
-                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
-                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
-                  Curabitur scelerisque placerat ultrices. Proin purus ante,
-                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
-                  orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam tincidunt arcu vel arcu fermentum, eget accumsan
+                    dolor dignissim. Nam eget quam semper, varius ligula sed,
+                    auctor augue. Maecenas et porta magna. Nulla mattis tortor
+                    vitae metus vestibulum scelerisque. Ut mollis a nisl vel
+                    suscipit. Curabitur scelerisque placerat ultrices. Proin
+                    purus ante, congue vel arcu ac, porta condimentum mauris.
+                    Maecenas massa orci, fringilla eget fermentum nec, dignissim
+                    nec diam.
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="1">
                   Nulla porttitor eros ac ex tristique posuere
                 </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                  dignissim. Nam eget quam semper, varius ligula sed, auctor
-                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
-                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
-                  Curabitur scelerisque placerat ultrices. Proin purus ante,
-                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
-                  orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                <Accordion.Collapse eventKey="1">
+                  <Card.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam tincidunt arcu vel arcu fermentum, eget accumsan
+                    dolor dignissim. Nam eget quam semper, varius ligula sed,
+                    auctor augue. Maecenas et porta magna. Nulla mattis tortor
+                    vitae metus vestibulum scelerisque. Ut mollis a nisl vel
+                    suscipit. Curabitur scelerisque placerat ultrices. Proin
+                    purus ante, congue vel arcu ac, porta condimentum mauris.
+                    Maecenas massa orci, fringilla eget fermentum nec, dignissim
+                    nec diam.
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="2">
                   Quisque fringilla tincidunt arcu
                 </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="2">
-                <Card.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                  dignissim. Nam eget quam semper, varius ligula sed, auctor
-                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
-                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
-                  Curabitur scelerisque placerat ultrices. Proin purus ante,
-                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
-                  orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="3">
+                <Accordion.Collapse eventKey="2">
+                  <Card.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam tincidunt arcu vel arcu fermentum, eget accumsan
+                    dolor dignissim. Nam eget quam semper, varius ligula sed,
+                    auctor augue. Maecenas et porta magna. Nulla mattis tortor
+                    vitae metus vestibulum scelerisque. Ut mollis a nisl vel
+                    suscipit. Curabitur scelerisque placerat ultrices. Proin
+                    purus ante, congue vel arcu ac, porta condimentum mauris.
+                    Maecenas massa orci, fringilla eget fermentum nec, dignissim
+                    nec diam.
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="3">
                   Nam elementum lobortis purus a maximus tortor
                 </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="3">
-                <Card.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor
-                  dignissim. Nam eget quam semper, varius ligula sed, auctor
-                  augue. Maecenas et porta magna. Nulla mattis tortor vitae
-                  metus vestibulum scelerisque. Ut mollis a nisl vel suscipit.
-                  Curabitur scelerisque placerat ultrices. Proin purus ante,
-                  congue vel arcu ac, porta condimentum mauris. Maecenas massa
-                  orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
-        </Col>
-      </Row>
-    </Container>
-  </section>
-);
+                <Accordion.Collapse eventKey="3">
+                  <Card.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam tincidunt arcu vel arcu fermentum, eget accumsan
+                    dolor dignissim. Nam eget quam semper, varius ligula sed,
+                    auctor augue. Maecenas et porta magna. Nulla mattis tortor
+                    vitae metus vestibulum scelerisque. Ut mollis a nisl vel
+                    suscipit. Curabitur scelerisque placerat ultrices. Proin
+                    purus ante, congue vel arcu ac, porta condimentum mauris.
+                    Maecenas massa orci, fringilla eget fermentum nec, dignissim
+                    nec diam.
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
 export default Faq;
